ab137719811e7424037c5f452c135a52fa10080c
tiger <biguptiger@gmail.com>
2023-06-04 21:51:20 +0800

    Chirpを保存する

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Response;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
@@ -29,9 +30,18 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        // リクエストから取得したデータをバリデーションします。'message'フィールドは必須で、文字列でなければならず、最大255文字である必要があります。
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        // 現在認証されているユーザー（$request->user()）が所有するchirpsに、バリデーション済みのデータを使って新たなchirpを作成します。
+        $request->user()->chirps()->create($validated);
+
+        // 処理が終わったら、ユーザーをchirps.indexルート（つぶやき一覧画面）へリダイレクトします。
+        return redirect(route('chirps.index'));
     }
 
     /**
